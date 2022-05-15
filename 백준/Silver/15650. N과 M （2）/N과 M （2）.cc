c131029ba957090e388a2042564b2d12300e000e
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-15 20:48:10 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ans[9];
+
+void go(int idx, int n, int m)
+{
+	if (idx > m)
+	{
+		for (int i = 1; i <= m; i++)
+		{
+			cout << ans[i];
+			if (i != m)
+				cout << ' ';
+			else
+			{
+				cout << '\n';
+				return;
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (i > ans[idx - 1])
+		{
+			ans[idx] = i;
+			go(idx + 1, n, m);
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int n, m; cin >> n; cin >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		ans[1] = i;
+		go(2, n, m);
+	}
+}
