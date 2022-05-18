859fd8af273074c1eee0f63e7a943e341ec63acb
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-19 00:46:03 +0900

    [Bronze III] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(){
+	int t;
+	scanf("%d", &t);
+	for(int i=1;i<=t;i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n",i, a + b);
+	}
+	return 0;
+}
