46e22e901670c00a0d501f39f59d5a692314a1c7
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-19 01:00:32 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int a, b, c, d, e, n;
+	scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+	printf("%d", (a * a + b * b + c * c + d * d + e * e) % 10);
+	return 0;
+}
