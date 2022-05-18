b97aa8e317ef6c09343a5926484729706f957c85
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-19 00:47:35 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int a[1001], d[1001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int n; cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	for (int i = 1; i <= n; i++) {
+		d[i] = a[i];
+		for (int j = 1; j < i; j++) {
+			if (a[i] > a[j] && d[i] < d[j] + a[i])
+				d[i] = d[j] + a[i];
+		}
+	}
+	int max;
+	for (int i = 1; i <= n; i++) {
+		if (i == 1)
+			max = d[i];
+		else {
+			if (d[i] > max)
+				max = d[i];
+		}
+	}
+	cout << max;
+	return 0;
+}
