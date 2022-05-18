2b4723f2c635b6b4fd70ceacd909d3990881d73d
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-19 01:02:34 +0900

    [Bronze IV] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	if (((n % 4 == 0) && (n % 100 != 0)) || (n % 400 == 0))
+		printf("1");
+	else
+		printf("0");
+	return 0;
+}
