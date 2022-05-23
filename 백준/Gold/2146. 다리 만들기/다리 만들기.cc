6bce136bd08f1587c402ff36ba5e64da8d432332
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-24 01:40:00 +0900

    [Gold III] Title: 다리 만들기, Time: 1252 ms, Memory: 6336 KB -BaekjoonHub

---
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int map[100][100];
+int dist[1001][1001] = { 0 };
+bool visited[100][100];
+queue <pair<int, int>> q;
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+int n, min_dist = 10001;
+
+void dfs(int x, int y, int cnt) {
+	visited[x][y] = true;
+	map[x][y] = cnt;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (0 <= nx && nx < n && 0 <= ny && ny < n && !visited[nx][ny] && map[nx][ny])
+			dfs(nx, ny, cnt);
+	}
+}
+
+// 섬의 가장자리인지 체크
+bool is_edge(int x, int y, int cnt) {
+	for (int i = 0; i < 4; i++) {
+		int cx = x + dx[i];
+		int cy = y + dy[i];
+		if (0 <= cx && cx < n && 0 <= cy && cy < n && map[x][y] == cnt && map[cx][cy] == 0)
+			return true;
+	}
+	return false;
+}
+
+void bfs(int x, int y, int cnt) {
+	q.push({ x, y });
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (0 <= nx && nx < n && 0 <= ny && ny < n && !visited[nx][ny]) {
+				// 바다 항해중
+				if (map[nx][ny] == 0) {
+					q.push({ nx, ny });
+					visited[nx][ny] = true;
+					dist[nx][ny] = dist[x][y] + 1;
+				}
+				// 다른 섬 도착
+				else if (map[nx][ny] != cnt) {
+					dist[nx][ny] = dist[x][y];
+					min_dist = min(dist[nx][ny], min_dist);
+					while (!q.empty())
+						q.pop();
+					return;
+				}
+			}
+			
+		}
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	cin >> n;
+	// 지도 입력받기
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+		}
+	}
+	// 섬에 번호 붙이기
+	int cnt = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j] && map[i][j])
+				dfs(i, j, cnt++);
+		}
+	}
+	// 다른 섬까지의 최단거리 구하기
+	int mini = 10001;
+	for (int i = 1; i < cnt; i++) {
+		for (int a = 0; a < n; a++) {
+			for (int b = 0; b < n; b++) {
+				if (is_edge(a, b, i)) {
+					memset(visited, false, sizeof(visited));
+					memset(dist, 0, sizeof(dist));
+					bfs(a, b, i);
+				}
+			}
+		}
+	}
+	cout << min_dist;
+}
