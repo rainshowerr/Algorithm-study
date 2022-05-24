3fd189e4d425b793cfb937c64dbf3302133e39c0
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-05-24 22:09:49 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2640 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -3,8 +3,8 @@ using namespace std;
 #include <queue>
 
 queue<int> q;
-int dist[100002] = { 0 };
-bool visited[100002] = { false };
+int dist[100001] = { 0 };
+bool visited[100001] = { false };
 int n, k;
 int ans;
 
@@ -17,7 +17,7 @@ void bfs() {
 		int go[3] = { n - 1, n + 1, 2 * n }; // 한 칸 앞, 한 칸 뒤, 두배 점프
 		for (int i = 0; i < 3; i++) {
 			int nx = go[i];
-			if (0 <= nx && nx <= 100001 && !visited[nx]) {
+			if (0 <= nx && nx <= 100000 && !visited[nx]) {
 				q.push(nx);
 				visited[nx] = true;
 				dist[nx] = dist[n] + 1;
