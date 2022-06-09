a1688eb894dbdd7db1f12149ac23cd830392dca1
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-06-09 17:16:49 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	long long n;
+	cin >> n;
+	cout << n * n << '\n';
+	cout << '2' << '\n';
+	return 0;
+}
