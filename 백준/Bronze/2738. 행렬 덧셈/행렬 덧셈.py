f76f062b6de071c1886124f65b562203149ff187
YOOHOO <88616418+YOOHOO0@users.noreply.github.com>
2022-06-16 01:08:25 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+arr1 = [list(map(int, input().split())) for _ in range(n)]
+arr2 = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        print(arr1[i][j] + arr2[i][j], end=' ')
+    print()
