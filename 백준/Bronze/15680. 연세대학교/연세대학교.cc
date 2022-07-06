36e5251dad72eadea615c1812b0a95199ee9c196
YOOHOO <yoohoo_030@naver.com>
2022-07-07 01:01:27 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    if (n == 0) cout << "YONSEI";
+    else if (n == 1) cout << "Leading the Way to the Future";
+}
