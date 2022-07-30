935bbb82134115483857cbd0aa90f4d297819b10
YOOHOO <yoohoo_030@naver.com>
2022-07-31 04:27:07 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cout << ".  .   ." << '\n';
+    cout << "|  | _ | _. _ ._ _  _" << '\n';
+    cout << "|/\\|(/.|(_.(_)[ | )(/.";
+}
