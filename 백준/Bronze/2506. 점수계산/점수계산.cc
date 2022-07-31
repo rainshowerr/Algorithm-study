c104c7c43b46139c356fa7d99b796758cffc2846
YOOHOO <yoohoo_030@naver.com>
2022-08-01 04:20:42 +0900

    [Bronze III] Title: 점수계산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n, arr[100], sum = 0, current = 1;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        if (k == 0)
+            current = 1;
+        else if (k == 1) {
+            sum += current;
+            current++;
+        }
+    }
+    cout << sum;
+}
