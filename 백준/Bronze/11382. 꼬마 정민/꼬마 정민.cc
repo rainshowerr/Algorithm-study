7cd7c337d2eff564dda525793149fc8cdd5e64e3
YOOHOO <yoohoo_030@naver.com>
2022-08-06 03:23:38 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    unsigned long long int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+}
