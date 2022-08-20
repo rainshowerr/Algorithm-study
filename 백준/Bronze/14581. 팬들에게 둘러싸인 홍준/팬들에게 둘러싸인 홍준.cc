d3e1dc7d76ea5b9540bb1dda4d39b159fff54ecd
YOOHOO <yoohoo_030@naver.com>
2022-08-21 04:53:59 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    string id;
+	cin >> id;
+	cout << ":fan::fan::fan:" << '\n';
+	cout << ":fan:" << ":" << id << ":" << ":fan:" << '\n';
+	cout << ":fan::fan::fan:" << '\n';
+}
