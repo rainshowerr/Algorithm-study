d09007648bc5ade3e01a88b5e7b62c600afbb76d
YOOHOO <yoohoo_030@naver.com>
2022-08-26 01:42:30 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	long long a, b;
+	cin >> a >> b;
+	if (a == b)	cout << 1;
+	else cout << 0;
+}
