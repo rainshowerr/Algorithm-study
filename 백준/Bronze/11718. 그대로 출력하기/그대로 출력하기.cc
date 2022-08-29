c7eee90b45689f14396e6acceb4a2e060affe71b
YOOHOO <yoohoo_030@naver.com>
2022-08-30 01:41:38 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	string s;
+	while(true) {
+		getline(cin, s);
+   		if (s=="")
+        	break;
+     	cout << s << endl;
+	}
+}
