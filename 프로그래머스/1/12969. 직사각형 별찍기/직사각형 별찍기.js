906188ca46440ffbf852fb9d57b5a7084d5e8b03
yoohoo <yoohoo_030@naver.com>
2024-06-25 10:53:45 +0900

    [level 1] Title: 직사각형 별찍기, Time: 98.42 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    for(var i = 0; i < b; i++) {
+        for(var j = 0; j < a; j++) {
+            process.stdout.write('*');
+        }
+        console.log()
+    }
+});
