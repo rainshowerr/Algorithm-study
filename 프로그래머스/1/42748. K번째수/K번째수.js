70a983cca38e833ab0c511fdcd369920187ffa6f
yoohoo <yoohoo_030@naver.com>
2024-07-05 19:04:26 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    var answer = [];
+    for(let i = 0; i < commands.length; i++) {
+        let subarr = array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => a - b);
+        answer.push(subarr[commands[i][2] - 1]);
+    }
+    return answer;
+}
