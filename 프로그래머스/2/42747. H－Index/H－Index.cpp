c501b3c0f4057cf03cb9415f0679591e30ee0fe1
yoohoo <yoohoo_030@naver.com>
2024-09-19 14:29:18 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int solution(vector<int> citations) {
     sort(citations.begin(), citations.end(), greater<>());
     for(int i = 0; i < citations.size(); i++) {
-        if (citations[i] < i + 1) {
-            return i;
+        if (citations[i] <= i + 1) {
+            return i + 1;
         }
     }
-    return citations.size();
+    return 0;
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,6 +1,6 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 19일 14:28:59
+2024년 09월 19일 14:29:16
 
 ### 문제 설명
 
