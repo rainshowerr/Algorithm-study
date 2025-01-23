a07ef35367432c5f05c9397803d9fc9273e0f34c
yoohoo <yoohoo030@gmail.com>
2025-01-23 18:50:17 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.00 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.88 ms
+메모리: 4.14 MB, 시간: 1.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 23일 17:16:55
+2025년 01월 23일 18:50:15
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -9,26 +9,25 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int current_weight = 0;
     queue<int> bridge;
 
-    // bridge를 [-1, -1, -1 ...]로 채움
+    // bridge를 [0, 0, 0 ...]로 채움
     for(int i = 0; i < bridge_length; i++)
-        bridge.push(-1);
+        bridge.push(0);
     
-    // 대기 트럭이 없고 bridge가 -1로 채워질 때까지 반복
+    // 대기 트럭이 없고 bridge가 0으로 채워질 때까지 반복
     while(!(truck_weights.empty() && current_weight == 0)) {
         // 1초마다 bridge pop
-        if (bridge.front() != -1) // -1이라면 빈 공간이므로 무게에 포함x
-            current_weight -= bridge.front();
+        current_weight -= bridge.front();
         bridge.pop();
 
-        // 대기 트럭이 있고 다리를 건널 수 있다면 bridge에 넣음
+        // 대기 트럭이 있고 다리를 건널 수 있다면 bridge에 push
         if (!truck_weights.empty() && current_weight + truck_weights.front() <= weight) {
             bridge.push(truck_weights.front());
             current_weight += truck_weights.front();
             truck_weights.erase(truck_weights.begin()); // 대기 트럭 벡터에서 삭제
         }
-        // 대기 트럭이 없거나 다리를 건널 수 없다면 -1을 bridge에 넣음
+        // 대기 트럭이 없거나 다리를 건널 수 없다면 bridge에 0을 push
         else
-            bridge.push(-1);
+            bridge.push(0);
         time++;
     }
     return time;
