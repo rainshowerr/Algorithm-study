6aa685628bc78d09ff321ea6c4a3b0208035fde4
yoohoo <yoohoo030@gmail.com>
2025-03-16 21:36:10 +0900

    [level 2] Title: 완전범죄, Time: 1.45 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int minn = 1e9, nn, mm, rows;
+int memo[121][121][41];
+
+void dfs(int a, int b, int k, vector<vector<int>>& info) {
+    if (a >= nn || b >= mm) return;
+    if (memo[a][b][k]) return;
+    if (k == rows) {
+        minn = min(minn, a);
+        return;
+    }
+    dfs(a + info[k][0], b, k + 1, info);
+    dfs(a, b + info[k][1], k + 1, info);
+    memo[a + info[k][0]][b][k + 1] = 1;
+    memo[a][b + info[k][1]][k + 1] = 1;
+}
+
+int solution(vector<vector<int>> info, int n, int m) {
+    nn = n; mm = m; rows = info.size();
+    dfs(0, 0, 0, info);
+    if (minn == 1e9) return -1;
+    else return minn;
+}
